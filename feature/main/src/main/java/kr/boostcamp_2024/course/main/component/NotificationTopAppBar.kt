87d445cfea40b87ffd7dd56af4fbe057997dc96c
mk43313@gmail.com <mk43313@gmail.com>
2025-01-15 22:54:10 +0900

    refactor: NotificationTopAppBar PreviewKoLightDark적용(#196)

--- feature/main/src/main/java/kr/boostcamp_2024/course/main/component/NotificationTopAppBar.kt
+++ feature/main/src/main/java/kr/boostcamp_2024/course/main/component/NotificationTopAppBar.kt
@@ -12,7 +12,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import kr.boostcamp_2024.course.designsystem.ui.annotation.PreviewKoLightDark
 import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
 import kr.boostcamp_2024.course.main.R
 
@@ -49,9 +49,9 @@ internal fun NotificationTopAppBar(
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true, locale = "ko")
+@PreviewKoLightDark
 @Composable
-fun NotificationTopAppBarPreview() {
+private fun NotificationTopAppBarPreview() {
     WeQuizTheme {
         NotificationTopAppBar(
             onNavigationButtonClick = {},
