bf1226906c031c4227cb7d9f1af5a6a8e690e20e
mk43313@gmail.com <mk43313@gmail.com>
2025-01-15 22:55:35 +0900

    refactor: CategoryListScreen PreviewKoLightDark적용(#196)

--- feature/study/src/main/java/kr/boostcamp_2024/course/study/presentation/CategoryListScreen.kt
+++ feature/study/src/main/java/kr/boostcamp_2024/course/study/presentation/CategoryListScreen.kt
@@ -12,8 +12,8 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kr.boostcamp_2024.course.designsystem.ui.annotation.PreviewKoLightDark
 import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
 import kr.boostcamp_2024.course.domain.model.Category
 import kr.boostcamp_2024.course.domain.model.StudyGroup
@@ -70,7 +70,7 @@ fun CategoryLazyColumn(
     }
 }
 
-@Preview(showBackground = true, locale = "ko")
+@PreviewKoLightDark
 @Composable
 fun CategoryListScreenPreview() {
     WeQuizTheme {
