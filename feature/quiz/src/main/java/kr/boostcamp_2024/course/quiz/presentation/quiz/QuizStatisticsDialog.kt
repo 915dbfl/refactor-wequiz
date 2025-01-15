cdf6aa2b4676df672ded8fd59e2dd29fc5ddfa64
mk43313@gmail.com <mk43313@gmail.com>
2025-01-15 22:49:21 +0900

    refactor: QuizStatisticsDialog PreviewKoLightDark 적용(#196)

--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/presentation/quiz/QuizStatisticsDialog.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/presentation/quiz/QuizStatisticsDialog.kt
@@ -13,8 +13,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kr.boostcamp_2024.course.designsystem.ui.annotation.PreviewKoLightDark
 import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
 import kr.boostcamp_2024.course.quiz.R
 import kr.boostcamp_2024.course.quiz.presentation.question.PieChartScreen
@@ -52,7 +52,7 @@ fun QuizStatisticsDialog(
     )
 }
 
-@Preview(showBackground = true, locale = "ko")
+@PreviewKoLightDark
 @Composable
 private fun QuizStaticsDialogPreview() {
     WeQuizTheme {
