a61bfad620bf8feae16c2afe9bcf6c2bef44e47c
mk43313@gmail.com <mk43313@gmail.com>
2025-01-15 22:56:49 +0900

    refactor: MainDropDownMenu PreviewKoLightDark적용(#196)

--- feature/main/src/main/java/kr/boostcamp_2024/course/main/component/MainDropDownMenu.kt
+++ feature/main/src/main/java/kr/boostcamp_2024/course/main/component/MainDropDownMenu.kt
@@ -1,12 +1,25 @@
 package kr.boostcamp_2024.course.main.component
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import kr.boostcamp_2024.course.designsystem.ui.annotation.PreviewKoLightDark
 import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
 import kr.boostcamp_2024.course.main.R
 
@@ -39,15 +52,25 @@ internal fun MainDropDownMenu(
     }
 }
 
-@Preview(showBackground = true, locale = "ko")
+@PreviewKoLightDark
 @Composable
 private fun MainDropDownMenuPreview() {
     WeQuizTheme {
-        MainDropDownMenu(
-            isExpanded = true,
-            onDismissRequest = {},
-            onEditUserClick = {},
-            onLogOutClick = {},
-        )
+        var expanded by remember { mutableStateOf(false) }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.TopStart),
+        ) {
+            IconButton(onClick = { expanded = true }) {
+                Icon(Icons.Default.MoreVert, contentDescription = "")
+            }
+            MainDropDownMenu(
+                isExpanded = expanded,
+                onDismissRequest = { expanded = false },
+                onEditUserClick = {},
+                onLogOutClick = {},
+            )
+        }
     }
 }
