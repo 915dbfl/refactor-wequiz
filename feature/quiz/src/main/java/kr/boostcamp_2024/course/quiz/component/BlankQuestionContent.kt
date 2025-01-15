5b75199747a8c1f3b12689a43f6c755d6bafefdc
mk43313@gmail.com <mk43313@gmail.com>
2025-01-15 22:50:51 +0900

    refactor: BlankQuestionContent PreviewKoLightDark적용(#196)

--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/component/BlankQuestionContent.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/component/BlankQuestionContent.kt
@@ -13,8 +13,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kr.boostcamp_2024.course.designsystem.ui.annotation.PreviewKoLightDark
 import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
 import kr.boostcamp_2024.course.domain.model.BlankQuestion
 import kr.boostcamp_2024.course.quiz.R
@@ -63,7 +63,7 @@ fun BlankQuestionTitle(
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun BlankQuestionContents(
+internal fun BlankQuestionContents(
     isOwner: Boolean,
     contents: List<Map<String, Any>?>,
     onRemoveContentClick: (Int) -> Unit,
@@ -163,7 +163,7 @@ fun QuestionContentLabel(
     )
 }
 
-@Preview(showBackground = true, locale = "ko")
+@PreviewKoLightDark
 @Composable
 private fun BlankQuestionContentPreview() {
     val question = BlankQuestion(
