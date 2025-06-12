fc1c3865d3a5f49b1cddd3c7868dc49b61254fc0
915dbfl <dbfl00915@naver.com>
2025-06-12 22:37:58 +0900

    [feat] BaseViewModel 정의(#2)
    하위 viewModel에서 강제적으로 재구현해야 하는 코드가 없기 때문에 단순 상속 가능한 class 형태로 선언

--- core/designsystem/src/main/java/kr/boostcamp_2024/course/designsystem/ui/base/BaseViewModel.kt
+++ core/designsystem/src/main/java/kr/boostcamp_2024/course/designsystem/ui/base/BaseViewModel.kt
@@ -1,4 +1,19 @@
 package kr.boostcamp_2024.course.designsystem.ui.base
 
-class BaseViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import kr.boostcamp_2024.course.domain.WeQuizException
+
+open class BaseViewModel: ViewModel() {
+    protected val _errorChannel = Channel<WeQuizException>()
+    val errorFlow = _errorChannel.receiveAsFlow()
+
+    protected fun handleError(messageId: Int? = null, throwable: Throwable? = null) {
+        viewModelScope.launch {
+            _errorChannel.send(WeQuizException.fromThrowable(messageId, throwable))
+        }
+    }
 }
